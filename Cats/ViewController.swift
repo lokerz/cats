e98d501e852865139e45b0eaa74ffc2307245458
Ridwan Hanif Abdurrasyid <ridwan.abdurrasyid@yahoo.com>
2023-08-31 16:53:04 +0700

    Initial Commit

---
+++ Cats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cats
+//
+//  Created by Ridwan Abdurrasyid on 31/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
